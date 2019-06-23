b29baaeea085af1744bcbb33659ee269b65718ad
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-06-22 23:57:39 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// jawsdb addon created
+// JAWSDB_URL saved in .env
+// NODE_MODULES_CACHE is set to false
+// Jawsdb databse `txtyyh8t8yp0ilyj` is all set with required table
+// Still deploying on heroku says
+// Error: Cannot find module './controllers/Burgers_Controller.js'
+// Works fine locally
 
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "root",
-//     database: "burgers_db"
-// });
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
 
 // Make connection.
 connection.connect(function (err) {
