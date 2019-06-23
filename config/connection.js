f40e69b8e0e6eebb6ab1d4a2eadb2fac04597ce7
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-06-22 23:26:15 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,14 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
+
 // Make connection.
 connection.connect(function (err) {
     if (err) {
