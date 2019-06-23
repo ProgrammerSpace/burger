ce0431ee6896badc390421609bfdb5a5f19af9cc
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-06-23 13:20:15 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -9,15 +9,15 @@ var mysql = require("mysql");
 // Error: Cannot find module './controllers/Burgers_Controller.js'
 // Works fine locally
 
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function (err) {
